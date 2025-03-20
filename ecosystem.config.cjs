67618c3dc2a12cfcdd4be53d0108476fb449ca11
dkshs <88355080+dkshs@users.noreply.github.com>
2025-03-20 17:53:55 -0300

    chore: use pm2 in the root of the repo

---
+++ ecosystem.config.cjs
@@ -0,0 +1,27 @@
+const defaultOpts = {
+  script: "./index.js",
+  env_production: {
+    NODE_ENV: "production",
+  },
+  env_development: {
+    NODE_ENV: "development",
+  },
+};
+
+module.exports = {
+  apps: [
+    {
+      name: "worker",
+      cwd: "./apps/worker/dist",
+      instances: process.env.WORKER_INSTANCES || 1,
+      ...defaultOpts,
+    },
+    {
+      name: "queue-dashboard",
+      cwd: "./apps/queue-dashboard/dist",
+      instances: process.env.DASHBOARD_INSTANCES || 1,
+      port: process.env.PORT || 3002,
+      ...defaultOpts,
+    },
+  ],
+};
